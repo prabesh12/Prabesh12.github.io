2c66d2fac09ebc8dd6abfc74abdc76c1c48333ff
PrabeshOli <herokaje886@gmail.com>
2022-01-03 00:57:06 +0545

    js object added

--- sporthouse/js/home .js
+++ sporthouse/js/home .js
@@ -1,5 +1,44 @@
 
+const productList = [
 
+  {
+ " id": 1,
+ title: "Mens Jersy",
+ name: "Jermany Jersy",
+ src: "img/18_21.jpg",
+ price: " price 500",
+ description:
+   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
+},
+{
+ " id": 2,
+ title: "Mens Jersy",
+ name: "Brazil Jersy",
+ src: "img/18_21.jpg",
+ price: " price 500",
+ description:
+   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
+},
+{
+ " id": 3,
+ title: "Mens Jersy",
+ name: "Portugal Jersy",
+ src: "img/18_21.jpg",
+ price: " price 500",
+ description:
+   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
+},
+{
+ " id": 4,
+ title: "Mens Jersy",
+ name: "hungary Jersy",
+ src: "img/18_21.jpg",
+ price: " price 500",
+ description:
+   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
+}
+
+]
 function creatProductGrdi(n){
   for (let i =0; i<n; i++){
     
