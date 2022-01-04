9036b1c06d8a77d99e15a3fbbb312252d9eb4c9f
PrabeshOli <herokaje886@gmail.com>
2022-01-04 15:19:46 +0545

    rendering the productimage and product info in single product page dynamically

--- SportHouse/js/list.js
+++ SportHouse/js/list.js
@@ -5,34 +5,34 @@ export const productList = [
    title: "Mens Jersy",
    name: "Jermany Jersy",
    src: "img/18_21.jpg",
-   price: " price 500",
+   price: " Rs. 500",
    description:
      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
   },
   {
     id: 2,
    title: "Mens Jersy",
-   name: "Brazil Jersy",
-   src: "img/18_21.jpg",
-   price: " price 500",
+   name: "Dumbells",
+   src: "img/img2.jpg",
+   price: " Rs. 3000/",
    description:
      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
   },
   {
    id: 3,
    title: "Mens Jersy",
-   name: "Portugal Jersy",
-   src: "img/18_21.jpg",
-   price: " price 500",
+   name: "Football Shoes",
+   src: "img/img3.jpg",
+   price: " Rs. 1500/",
    description:
      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
   },
   {
    id: 4,
    title: "Mens Jersy",
-   name: "hungary Jersy",
-   src: "img/18_21.jpg",
-   price: " price 500",
+   name: "Mass Gainer",
+   src: "img/img4.jpg",
+   price: " Rs. 3000/",
    description:
      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde vero. Corporis, vero. Doloremque consequuntur, tenetur magni obcaecati distinctio officiis blanditiis recusandae aspernatur illo ipsam quam ex earum dolorum facere.",
   }
